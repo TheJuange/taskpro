367185eeb1efcdc579c0d278ee2a1868c9d65bdf
Gerardo <gyumacales@gmail.com>
2025-01-26 13:46:32 -0400

    TaskPro Project Initialization

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TaskPro</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-neutral-900">
+    <nav class="w-full fixed top-0 left-0 flex items-center justify-between p-4 bg-neutral-800">
+        <h1 class="text-neutral-300 font-semibold text-3xl">TaskPro</h1>
+        <ul class="flex items-center space-x-4">
+            <a href="{{route("create")}}" class="text-white font-semibold text-sm bg-green-500 rounded-md p-2" href="">New Task</a>
+            <a class="text-neutral-300 text-sm font-semibold" href="">github</a>
+        </ul>
+    </nav>
+    <main class="flex items-center justify-center h-screen">
+        <picture class="relative h-4/6  md:h-4/6 px-3">
+            <img class="rounded-3xl w-full h-full object-cover" src="{{asset("./images/fondo.png")}}" alt="photo">
+            <p class="text-white font-semibold text-2xl md:text-6xl absolute bottom-20 left-10">A place to log your tasks</p>
+            <a href="{{route("create")}}" class="text-white font-semibold text-sm bg-green-500 rounded-md p-2 absolute bottom-5 left-10" href="">Create New Task</a>
+        </picture>
+    </main>
+    @vite('resources/js/app.js')
+</body>
+</html>
---
+++ resources/views/task/create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TaskPro</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-neutral-900">
+    <nav class="w-full fixed top-0 left-0 flex items-center justify-between p-4 bg-neutral-800">
+        <a href="/" class="text-neutral-300 font-semibold text-3xl">TaskPro</a>
+        <ul class="flex items-center space-x-4">
+            <a href="{{route("show")}}" class="text-neutral-300 text-sm font-semibold">Task List </a>
+            <a class="text-neutral-300 text-sm font-semibold" href="">github</a>
+        </ul>
+    </nav>
+    <div class="w-full px-4 md:w-1/2 container mx-auto mt-20 flex justify-center">
+        <form action="" class="bg-neutral-800 p-4 rounded-md flex flex-col w-full mt-9 mb-9" method="POST">
+            <h1 class="text-neutral-300 text-center font-semibold text-3xl">Create Task</h1>
+            <label class="text-neutral-300 mb-2" for="title">Name Task</label>
+            <input class="p-2 rounded-md outline-none mb-4" type="text" name="title" id="title">
+
+            <label class="text-neutral-300 mb-2" for="title">Start Date</label>
+            <input class="p-2 rounded-md outline-none mb-4" type="date" name="title" id="title">
+
+            <label class="text-neutral-300 mb-2" for="description">Description</label>
+            <textarea class="p-2 rounded-md outline-none mb-4" name="description" id="description" cols="30"
+                rows="5"></textarea>
+
+            <label class="text-neutral-300 mb-2" for="priority">Priority</label>
+            <select class="p-2 rounded-md outline-none mb-4" name="priority" id="priority">
+                <option value="" disabled selected>Select an option</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+            <label class="text-neutral-300 mb-2" for="">Photo <span class="text-sm text-neutral-500">(Optional)</span></label>
+            <input class="p-2 rounded-md outline-none mb-4 text-neutral-300 bg-neutral-700" type="file" name="photo" id="photo">
+
+            <button class="bg-green-500 rounded p-2 uppercase text-white font-semibold" type="submit">Create
+                Task</button>
+        </form>
+    </div>
+</body>
+
+</html>
